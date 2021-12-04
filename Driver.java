e5ec82fe420a8eb49869ea705ebd30d38d4d618c
Sam Parkinson <43306113+sam-parkinson@users.noreply.github.com>
2021-12-04 02:56:35 -0500

    Finish day 1

--- Driver.java
+++ Driver.java
@@ -3,7 +3,8 @@ public class Driver
     public static void main(String[] args)
     {
         SonarSweep sweeper = new SonarSweep("day1input.txt");
-
+        
         System.out.println("Day 1 Problem 1: " + sweeper.getIncreases());
+        System.out.println("Day 1 Problem 2: " + sweeper.getWindowIncreases());
     }
 }
--- SonarSweep.java
+++ SonarSweep.java
@@ -1,17 +1,19 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class SonarSweep
+public class SonarSweep 
 {
     private int increases;
-    private int[] measurments;
+    private int windowIncreases;
+    private int[] measurements;
 
     public SonarSweep(String address)
     {
-        this.measurements = makeMeasurments(address);
+        this.measurements = makeMeasurements(address);
         this.increases = findIncreases();
-        // call method that turns text input into array
-        // call method that turns array into total
+        this.windowIncreases = findWindowIncreases();
     }
 
     // Getters
@@ -20,18 +22,78 @@ public class SonarSweep
         return this.increases;
     }
 
-    // Method that turns text input into array
-    private int[] makeMeasurments(String address)
+    public int getWindowIncreases()
     {
-        File file = new File(address);
-        Scanner stdin = new Scanner(file);
-        stdin.close();
-        return (0);
+        return this.windowIncreases;
+    }
+
+    // Method that turns text input into array
+    private int[] makeMeasurements(String address)
+    {  
+        ArrayList<Integer> measureList = new ArrayList();
+        try
+        {
+            File file = new File(address);
+            Scanner stdin = new Scanner(file);
+            
+
+            while (stdin.hasNext())
+            {
+                measureList.add(stdin.nextInt());
+            }
+
+            stdin.close();
+         
+        } catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+        int[] measurements = new int[measureList.toArray().length];
+
+        for (int i = 0; i < measurements.length; i++)
+        {
+            measurements[i] = (int) measureList.get(i);
+        }
+
+        return measurements;
     }
 
     // Method that processes array
     private int findIncreases()
     {
-        return this.measurments[0];
+        int increases = 0;
+        for (int i = 0; i < measurements.length - 1; i++)
+        {
+            if (measurements[i + 1] > measurements[i])
+                increases++;
+        }
+        return increases;
+    }
+    
+    // Note: could just compare i with i + 3?
+    private int findWindowIncreases()
+    {
+        int windowIncreases = 0;
+        
+        for (int i = 0; i < measurements.length - 3; i++)
+        {
+            /*
+            int a = 0, b = 0;
+
+            for (int j = i; j < i + 3; j++)
+                a += measurements[j];
+            
+            for (int j = i + 1; j < i + 4; j++)
+                b += measurements[j];
+
+            if (b > a)
+                windowIncreases++;
+            */
+            if (measurements[i + 3] > measurements[i])
+                windowIncreases++;
+        }
+
+        return windowIncreases;
     }
 }
